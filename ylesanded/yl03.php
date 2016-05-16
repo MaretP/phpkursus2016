372b72aa7cb05edea96f11135d3aec2e4f8cf78c
MaretP <maret.piiriste@mail.ee>
2016-05-16 15:01:40 +0300

    Update yl03.php

---
+++ ylesanded/yl03.php
@@ -0,0 +1,15 @@
+<?php 
+#Ülesanne 0301
+$mas = array("Karu", "Jänes", "Hunt", "Rebane", "Hirv", "Hiir", "Põder", "Ahv", "Mäger", "Mutt" );
+
+for ($i = 0; $i < sizeof($mas); $i++)
+{
+    
+}    
+foreach ($mas as $key => $val)
+{
+    echo "$key. $val<br>";
+}
+    echo "<p>";
+#Ülesanne 0302
+?>
